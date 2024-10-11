5d90bf64c6a64537cb7adeb414f703108048d6ec
Kyle Siler <kylesiler@gmail.com>
2024-10-10 22:57:16 -0700

    Update restapi to be async

--- http-client/httpclient.go
+++ http-client/httpclient.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 type CatFact struct {
@@ -12,8 +14,9 @@ type CatFact struct {
 	Type string
 }
 
-func main() {
-	resp, err := http.Get("https://cat-fact.herokuapp.com/facts/random")
+func callAPI(location string, endpoint string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	resp, err := http.Get(endpoint)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -23,11 +26,13 @@ func main() {
 		fmt.Println(err.Error())
 	}
 
-	// fact := new(CatFact)
-	var fact CatFact
-	json.Unmarshal(body, &fact)
+	fileName := "data/" + location + "-" + time.Now().Format("20060102") + ".json"
+	os.WriteFile(fileName, body, 0666)
+}
 
-	fmt.Println(fact.Text)
-	fmt.Println("")
-	fmt.Println(string(body))
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go callAPI("roseville", "https://www.autonationchryslerdodgejeepramroseville.com/apis/widget/INVENTORY_LISTING_DEFAULT_AUTO_NEW:inventory-data-bus1/getInventory?bodyStyle=Truck%20Crew%20Cab&compositeType=new&make=Jeep&model=Gladiator&year=2024&pageSize=50", &wg)
+	wg.Wait()
 }
